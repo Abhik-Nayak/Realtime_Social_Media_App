56cb5b4d675ffffb4bdabb85296b8f27272e1fa2
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2023-05-14 16:46:54 +0530

    Abhik//14-05-2023

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const env = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+
+env.config();
+
+mongoose.connect("mongodb://localhost:27017/realtime_social_media", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    family: 4,
+}).then(() => {
+    console.log("connected");
+}).catch(err => console.log(err));
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
+
+const userRoutes = require("./routes/user");
+app.use("/api/user",userRoutes);
